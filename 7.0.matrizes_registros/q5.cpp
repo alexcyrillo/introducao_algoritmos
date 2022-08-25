ac4d69405f5437846e9d53cc034352a37f5dfd0a
Alex Cyrillo <alex.csborges@hotmail.com>
2022-08-25 13:45:17 -0300

    exercicios feitos

--- 7.0.matrizes_registros/q5.cpp
+++ 7.0.matrizes_registros/q5.cpp
@@ -3,6 +3,34 @@ using namespace std;
 
 int main()
 {
-
+    int x;
+    cin >> x;
+    int triangPasc[x][x] = {0};
+    for(int l = 0; l < x; l++)
+    {
+        for(int c = 0; c < x; c++)
+        {
+            if(l <= c)
+            {
+                if(c == 0 || l == c)
+                {
+                    triangPasc[l][c] = 1;
+                }
+                else if(l > 0)
+                {
+                    triangPasc[l][c] = triangPasc[l - 1][c] + triangPasc[l - 1][c - 1];
+                }
+            }
+        }
+    }
+    for(int l = 0; l < x; l++)
+    {
+        for(int c = 0; c < x; c++)
+        {
+            if(triangPasc[l][c] != 0)
+            cout << triangPasc[l][c] << ' ';
+        }
+        cout << endl;
+    }
     return 0;
 }
